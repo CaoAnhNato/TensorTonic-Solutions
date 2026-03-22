f78659792c2d2cc1497221d2bc2e5feb5ef4c158
CaoAnhNato <caoanhdoan130605@gmail.com>
2026-03-22 12:52:11 +0700

    Add solution for bert-wordpiece

---
+++ bert/bert-wordpiece/bert-wordpiece.py
@@ -0,0 +1,49 @@
+from typing import List, Dict
+
+class WordPieceTokenizer:
+    """
+    WordPiece tokenizer for BERT.
+    """
+    
+    def __init__(self, vocab: Dict[str, int], unk_token: str = "[UNK]", max_word_len: int = 100):
+        self.vocab = vocab
+        self.unk_token = unk_token
+        self.max_word_len = max_word_len
+    
+    def tokenize(self, text: str) -> List[str]:
+        """
+        Tokenize text into WordPiece tokens.
+        """
+        tokens = []
+        for word in text.lower().split():
+            word_tokens = self._tokenize_word(word)
+            tokens.extend(word_tokens)
+        return tokens
+    
+    def _tokenize_word(self, word: str) -> List[str]:
+        """
+        Tokenize a single word into subwords.
+        """
+        if len(word) > self.max_word_len:
+            return [self.unk_token]
+        
+        output_tokens = []
+        start = 0
+        while start < len(word):
+          end = len(word)
+          curr_substr = None
+
+          while start < end:
+            substr = word[start:end]
+
+            if start > 0:
+              substr = '##' + substr
+            if substr in self.vocab:
+              curr_substr = substr
+              break
+            end -= 1
+          if curr_substr is None:
+            return [self.unk_token]
+          output_tokens.append(curr_substr)
+          start = end
+        return output_tokens
